e6cf075f80cad41d82cc50eb368f15d099e39448
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-09 23:33:29 +0530

    Hero Section Initialized

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import NavBar from '../fragments/NavBar'
 
 const HeroSection = () => {
-  return <div>HeroSection</div>
+  return (
+    <>
+      <NavBar />
+      HeroSection
+    </>
+  )
 }
 
 export default HeroSection
