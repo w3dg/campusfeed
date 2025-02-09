40459f724ada855ca748adb4036b1f769159ed7b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-09 23:31:48 +0530

    Hero Section Initialized

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,6 @@
+import HeroSection from '@/components/sections/HeroSection'
 import { Button } from '@heroui/react'
 
 export default function Landing() {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  )
+  return <HeroSection />
 }
