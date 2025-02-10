d7c3cda28f7d44894df383388e4bc5b66c771c8a
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-10 11:05:26 +0530

    Hero Section Initialized

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -5,7 +5,14 @@ const HeroSection = () => {
   return (
     <>
       <NavBar />
-      HeroSection
+      <section
+        className="min-h-[90vh] w-[90%] md:w-[80%] mx-auto py-12 rounded-[30px] border-2 border-black text-center"
+        style={{
+          background: 'linear-gradient(to bottom, #FCFDFD, #D3DEE3 100%)',
+        }}
+      >
+        <h1>Hello World</h1>
+      </section>
     </>
   )
 }
