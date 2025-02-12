9b13b65e5ed01a8242f28a72316a58909ffe9a8e
Sohom Mondal <sohommondalofficial@gmail.com>
2025-02-12 16:00:52 +0530

    refactor: enhance Features section layout and styling

--- components/sections/Features.jsx
+++ components/sections/Features.jsx
@@ -1,8 +1,10 @@
 "use client";
+
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Features() {
+
   const [features] = useState([
     {
       title: "One Platform for All",
@@ -27,7 +29,7 @@ export default function Features() {
   return (
     <section
       id="features"
-      className="relative isolate py-24 sm:py-32 px-6 lg:px-8"
+      className="relative isolate overflow-hidden px-6 lg:px-8"
     >
       <div
         aria-hidden="true"
@@ -38,50 +40,48 @@ export default function Features() {
           alt="ellipse"
           width={1440}
           height={578}
-          layout="responsive"
           className="relative opacity-30 right-[calc(50%-11rem)] aspect-square w-[36.125rem] translate-x-1/2 sm:right-[calc(50%-45rem)] sm:w-[60.1875rem] "
         />
       </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h6 className="text-base/normal font-sans mb-2 hidden md:block font-normal text-slate-600 uppercase">
-          What sets us apart
-        </h6>
-        <h1 className="text-5xl font-semibold font-sans text-pretty">
-          Features
-        </h1>
-        <h3 className="mt-6 text-7 font-sans text-slate-600">
-          We provide a wide range of features to make your experience better.
-        </h3>
-      </div>
-      <div className="flex flex-col items-center justify-center min-h-[50vh] py-2">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-center lg:grid-cols-3">
-          {features.map((feature) => (
-            <div key={feature.title} className="rounded-lg shadow-md p-6">
-              <div className="rounded-lg p-2 bg-[#8690B1]">
-                <img
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-2xl text-center">
+          <h6 className="text-base/normal font-sans mb-2 hidden md:block font-normal text-slate-600 uppercase">
+            What sets us apart
+          </h6>
+          <h1 className="text-5xl font-semibold font-sans text-pretty">
+            Features
+          </h1>
+          <h3 className="mt-6 text-7 font-sans text-slate-600">
+            We provide a wide range of features to make your experience better.
+          </h3>
+        </div>
+        <div className="mt-6 flex flex-col items-center justify-center min-h-[50vh] py-2">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:grid-cols-3">
+            {features.map((feature, index) => (
+              <div key={index} className="rounded-2xl shadow-xl border border-[#6DA27D] p-8 bg-background text-center">
+                <Image
                   src={feature.icon}
-                  className="flex"
-                  height={40}
-                  width={40}
+                  height={50}
+                  width={50}
                   alt="icon"
+                  className="object-contain shrink-0 p-3 bg-slate-100 shadow-inner rounded-2xl mx-auto mb-4"
                 />
+                <h2 className="text-2xl font-bold mb-4">{feature.title}</h2>
+                <p className="text-gray-700 text-sm">{feature.description}</p>
               </div>
-              <h2 className="text-2xl font-bold mb-4">{feature.title}</h2>
-              <p className="text-gray-700">{feature.description}</p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-60rem)]"
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]"
       >
         <Image
           src="/ellipse.svg"
           alt="ellipse"
           width={1440}
           height={578}
-          layout="responsive"
           className="relative opacity-50 right-[calc(50%+3rem)] aspect-square w-[36.125rem] translate-x-1/2 sm:right-[calc(50%+36rem)] sm:w-[72.1875rem]"
         />
       </div>
