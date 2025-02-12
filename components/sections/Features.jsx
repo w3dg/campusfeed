96744cf143fef0f75d29742f906254d761d56edb
Supreet0 <supreetpatel2004@gmail.com>
2025-02-12 19:05:23 +0530

    Just changes

--- components/sections/Features.jsx
+++ components/sections/Features.jsx
@@ -4,24 +4,23 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Features() {
-
   const [features] = useState([
     {
       title: "One Platform for All",
       description:
-        "A single, unified space for all your announcements and updates! It brings together academic announcements, event alerts, and society recruitments in one convenient location. Whether you're a student, teacher, or administrator, access everything you need in one place-quickly and efficiently.",
+        "A central hub for all announcements, featuring exciting event notifications, and opportunities for society recruitment, designed to be easily accessible for students, teachers alike.",
       icon: "globe.svg",
     },
     {
       title: "Realtime Updates",
       description:
-        "Stay ahead with instant notifications! It delivers real-time updates, ensuring users never miss an important announcement. Whether it's event schedules, academic notices or society recruitments you'll get the latest information as soon as it's published.",
+        "Stay informed with instant notifications that deliver real-time updates on crucial announcements, exciting events, and important academic notices, ensuring you never miss a key moment or opportunity.",
       icon: "clock.svg",
     },
     {
       title: "Easy to Manage",
       description:
-        "It is designed for effortless management. With a user-friendly interface, you can quickly post, edit, and organize announcements without any hassle. Whether you're an admin, teacher, or student, keeping track of important updates or announcements has never been easier.",
+        "It simplifies announcement management. With a user-friendly interface, you can quickly post, edit, and organize updates. It's easy for admins, teachers, and students to keep track of important information.",
       icon: "settings.svg",
     },
   ]);
@@ -40,7 +39,7 @@ export default function Features() {
           alt="ellipse"
           width={1440}
           height={578}
-          className="relative opacity-30 right-[calc(50%-11rem)] aspect-square w-[36.125rem] translate-x-1/2 sm:right-[calc(50%-45rem)] sm:w-[60.1875rem] "
+          className="relative opacity-50 right-[calc(50%-11rem)] aspect-square w-[36.125rem] translate-x-1/2 sm:right-[calc(50%-45rem)] sm:w-[60.1875rem] "
         />
       </div>
       <div className="py-24 sm:py-32">
@@ -58,16 +57,21 @@ export default function Features() {
         <div className="mt-6 flex flex-col items-center justify-center min-h-[50vh] py-2">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:grid-cols-3">
             {features.map((feature, index) => (
-              <div key={index} className="rounded-2xl shadow-xl border border-[#6DA27D] p-8 bg-background text-center">
+              <div
+                key={index}
+                className="rounded-2xl shadow-xl p-8 bg-background text-center"
+              >
                 <Image
                   src={feature.icon}
                   height={50}
                   width={50}
                   alt="icon"
-                  className="object-contain shrink-0 p-3 bg-slate-100 shadow-inner rounded-2xl mx-auto mb-4"
+                  className="object-contain shrink-0 p-3 bg-[#8690b1] shadow-inner rounded-2xl mx-auto mb-4 fill-white"
                 />
                 <h2 className="text-2xl font-bold mb-4">{feature.title}</h2>
-                <p className="text-gray-700 text-sm">{feature.description}</p>
+                <p className="text-gray-700 text-base text-left">
+                  {feature.description}
+                </p>
               </div>
             ))}
           </div>
