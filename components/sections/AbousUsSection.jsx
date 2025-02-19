9f07afab0499755784afad3b5853ea6df05fe0c1
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-19 20:12:21 +0530

    feat: Update About Us section with enhanced styling and additional description

--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -4,8 +4,9 @@ import ImageCard from '../fragments/Card';
 const AboutUsSection = () => {
   return (
     <section className="py-10 bg-gray-100 px-10">
-      <div className="container mx-auto text-center">
-        <h1 className="text-3xl font-bold text-gray-900 mb-6">Meet Our Team</h1>
+      <div className="container mx-auto text-center w-full max-w-[800px] mt-8">
+        <h1 className="text-5xl font-bold text-gray-900 mb-6">About Us</h1>
+        <h1 className="text-xl text-gray-400 mb-20 text-center">Meet the developers and designers behind this awesome project</h1>
         <div className="flex flex-wrap justify-center gap-10 items-start">
           {teamData.map((member) => (
             <ImageCard
