c6624ea9ee659e0f5cf8454a78f855e6cd4a4a55
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-19 15:42:38 +0530

    feat: Add About Us section and improve ImageCard styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import AboutUsSection from "@/components/sections/AbousUsSection";
 import Features from "@/components/sections/Features";
 
 export default function Landing() {
   return (
     <div>
       <Features />
+      <AboutUsSection/>
     </div>
   );
 }
--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -1,12 +1,12 @@
-import * as teamData from "/data/teamData.json"
+import teamData from "@/public/data/teamData.json";
 import ImageCard from '../fragments/Card';
 
 const AboutUsSection = () => {
   return (
-    <section className="py-10 bg-gray-100">
+    <section className="py-10 bg-gray-100 px-10">
       <div className="container mx-auto text-center">
         <h1 className="text-3xl font-bold text-gray-900 mb-6">Meet Our Team</h1>
-        <div className="flex flex-wrap justify-center gap-6 items-center">
+        <div className="flex flex-wrap justify-center gap-10 items-start">
           {teamData.map((member) => (
             <ImageCard
               key={member.id}
