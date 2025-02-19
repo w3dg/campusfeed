f00ac938541d23d6348fa0a356278d8924a29a57
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-19 21:09:09 +0530

    Event Page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,7 +1,12 @@
+'use client'
+import EventCard from '@/components/fragments/EventCard'
 import NavBar from '@/components/fragments/NavBar'
 import Image from 'next/image'
+import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Home() {
+  const [search, setSearch] = useState(false)
   return (
     <>
       <NavBar />
@@ -12,7 +17,7 @@ export default function Home() {
             <button className="bg-[#6DA27D] rounded-md p-2 text-white">
               Filter
             </button>
-            <button button className="bg-[#6DA27D] rounded-md p-2 text-white">
+            <button className="bg-[#6DA27D] rounded-md p-2 text-white">
               Sort By
             </button>
             <div className="flex gap-1 items-center bg-[#D3DEE3] px-3 py-1 rounded-md min-w-[250px]">
@@ -29,8 +34,70 @@ export default function Home() {
               />
             </div>
           </div>
+          <AnimatePresence>
+            {!search ? (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="mr-4 rounded-full bg-[#D3DEE3] p-2 md:hidden"
+              >
+                <Image
+                  src={'/SearchIcon.svg'}
+                  height={20}
+                  width={20}
+                  alt="search"
+                  onClick={() => setSearch(!search)}
+                />
+              </motion.div>
+            ) : (
+              <motion.div className="relative md:hidden">
+                <motion.input
+                  initial={{ width: 0, opacity: 0 }}
+                  animate={{ width: 'auto', opacity: 1 }}
+                  exit={{ width: 0, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  placeholder="Search "
+                  type="text"
+                  className="bg-[#D3DEE3] py-1 pl-3 pr-8 rounded-2xl placeholder:text-black/45 placeholder:text-sm placeholder:font-light focus:outline-none"
+                />
+                <button
+                  onClick={() => setSearch(false)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+                </button>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </header>
-        <div className="border-2 border-black mt-3 px-4 md:px-0"></div>
+        <div className="mt-3 px-0 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-2 md:gap-x-4 gap-y-10">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+        </div>
       </section>
     </>
   )
--- components/fragments/EventCard.jsx
+++ components/fragments/EventCard.jsx
@@ -1,30 +1,35 @@
-const EventCard = ({item}) =>{
-    return(
-    <div className="flex flex-col ">
-            <div
-              className="h-[70%] rounded-md 2xl:rounded-[18.5px] overflow-hidden relative  
-             md:h-[80%] 2xl:h-[50vh] border-[5px] border-slate-200 bg-no-repeat bg-cover bg-center"
-             style={{ backgroundImage: `url(${item})` }}
-             ></div>
-            <div className=" h-[22%] rounded-md mt-[4px] flex gap-1 relative md:mt-[6px] 2xl:h-[0px]">
-              <div className="flex flex-col text-[5px] rounded-[2px] overflow-hidden w-3 md:w-7 md:rounded-md">
-                <div className="bg-gray-500  h-[45%] text-[4px] text-white pt-[1px] md:text-[8px] md:flex md:justify-center md:items-center">
-                  Sat
-                </div>
-                <div className="bg-gray-300  h-[70%] text-[4px]/[4px] pt-[1px] md:text-[8px] md:flex md:justify-center md:items-center md:p-[3px] md:leading-[8px]">
-                  27 Mar
-                </div>
-              </div>
-              <div className="flex flex-col gap-[1px] items-start md:gap-[6px]">
-                <div className="text-[5px] h-[5px] md:text-[9px]">Name</div>
-                <div className="text-[5px] h-[5px] md:text-[9px]">Location</div>
-                <div className="text-[5px] h-[5px] text-gray-400 md:text-[9px]">
-                  Amount
-                </div>
-              </div>
-            </div>
-          </div>
-          )
+import Image from 'next/image'
+
+const EventCard = () => {
+  return (
+    <div className="flex flex-col">
+      <Image
+        src={'/EventDemo.svg'}
+        alt="event"
+        width={150}
+        height={150}
+        className="h-[150px] w-[150px] md:w-[180px] md:h-[180px] lg:w-[250px] lg:h-[250px] object-contain border-[5px] border-[D3DEE3] rounded-2xl"
+      />
+      <div className="flex  mt-2 gap-1">
+        <div className="flex flex-col items-center text-center  rounded-md ">
+          <p className="text-[9px] bg-[#8690B1] px-3 py-1 rounded-t-md text-[#FCFDFD] min-w-full">
+            Sat
+          </p>
+          <p className="text-[14px] px-3 bg-[#E7EDF0] min-w-fit">27</p>
+          <p className="text-[10px] px-3 rounded-b-md bg-[#E7EDF0] min-w-full">
+            Mar
+          </p>
+        </div>
+        <div className="flex flex-col text-[12px] ">
+          <p className="font-medium line-clamp-1">
+            KODESPHERE - The Flagship Event
+          </p>
+          <p className="font-medium">Campus 15</p>
+          <p className="text-black/50"> Rs.200</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default EventCard;
+export default EventCard
