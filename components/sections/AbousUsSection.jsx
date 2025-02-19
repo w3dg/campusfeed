44d5082790b67528f1239aa9af72c98587dbe2a8
Supreet0 <supreetpatel2004@gmail.com>
2025-02-19 22:04:18 +0530

    About us some changes

--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -1,5 +1,6 @@
 import teamData from "@/public/data/teamData.json";
 import ImageCard from "../fragments/Card";
+import Image from "next/image";
 
 const AboutUsSection = () => {
   return (
@@ -7,12 +8,26 @@ const AboutUsSection = () => {
       id="about-us"
       className="relative isolate overflow-hidden px-6 lg:px-6"
     >
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-96"
+      >
+        <Image
+          src="/ellipse.svg"
+          alt="ellipse"
+          width={1440}
+          height={578}
+          className="relative right-[calc(50%-11rem)] aspect-square w-[36.125rem] translate-x-[100%] opacity-50 sm:right-[calc(50%-45rem)] sm:w-[60.1875rem]"
+        />
+      </div>
       <div className="py-24 sm:py-32">
         <div className="mx-auto max-w-2xl text-center">
-          <h1 className="mb-6 text-5xl font-bold text-gray-900">About Us</h1>
-          <h1 className="mb-12 text-center text-xl text-gray-400">
-            Meet the developers and designers behind this awesome project
+          <h1 className="text-pretty font-sans text-5xl font-semibold">
+            Meet Our Team
           </h1>
+          <h3 className="text-7 mt-6 font-sans text-slate-600">
+            Meet the developers and designers behind this awesome project.
+          </h3>
         </div>
         <div className="mt-6 flex min-h-[50vh] flex-col items-center justify-center py-2">
           <div className="grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-4">
