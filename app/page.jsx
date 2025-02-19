9d55f2442f3a5d60deba03ce02d89e8a4630649c
Sahil Choudhary <sahil844101@gmail.com>
2025-02-19 21:00:36 +0530

    Fixed the merge conflict due to page.jsx file

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
-import { Button } from "@heroui/react";
 import FooterSection from "@/components/sections/FooterSection";
 
 export default function Landing() {
   return (
     <div>
-      <Button className="border-black border rounded bg-yellow-400">Hello World</Button>
-      <FooterSection />
+      <FooterSection/>
     </div>
   );
 }
