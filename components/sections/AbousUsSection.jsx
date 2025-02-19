0fd8d06a548eb58a040cde4d26134da127b79fa4
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-19 20:40:20 +0530

    refactor: Improve layout and styling of ImageCard and AboutUsSection components

--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -1,41 +1,33 @@
 import teamData from "@/public/data/teamData.json";
-import ImageCard from '../fragments/Card';
+import ImageCard from "../fragments/Card";
 
 const AboutUsSection = () => {
   return (
-<<<<<<< HEAD
     <section
       id="about-us"
       className="relative isolate overflow-hidden px-6 lg:px-6"
     >
       <div className="py-24 sm:py-32">
         <div className="mx-auto max-w-2xl text-center">
-          <h1 className="text-5xl font-semibold font-sans text-pretty">
-            Meet Our Team
+          <h1 className="mb-6 text-5xl font-bold text-gray-900">About Us</h1>
+          <h1 className="mb-12 text-center text-xl text-gray-400">
+            Meet the developers and designers behind this awesome project
           </h1>
         </div>
-       <div className="mt-6 flex flex-col items-center justify-center min-h-[50vh] py-2">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:grid-cols-4">
-                 {teamData.map((member) => (
-=======
-    <section className="py-10 bg-background px-10">
-      <div className="container mx-auto text-center w-full max-w-[800px] mt-8">
-        <h1 className="text-5xl font-bold text-gray-900 mb-6">About Us</h1>
-        <h1 className="text-xl text-gray-400 mb-20 text-center">Meet the developers and designers behind this awesome project</h1>
-        <div className="flex flex-wrap justify-center gap-10 items-start">
-          {teamData.map((member) => (
->>>>>>> 46b6eafbaf816e41f4c654fa86c4bb6c93ca8c9a
-            <ImageCard
-              key={member.id}
-              title={member.title}
-              subtitle={member.subtitle}
-              image={member.image}
-              github={member.github}
-            />
-          ))}
-            </div>
+        <div className="mt-6 flex min-h-[50vh] flex-col items-center justify-center py-2">
+          <div className="grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-4">
+            {teamData.map((member) => (
+              <ImageCard
+                key={member.id}
+                title={member.title}
+                subtitle={member.subtitle}
+                image={member.image}
+                github={member.github}
+              />
+            ))}
           </div>
-       </div>
+        </div>
+      </div>
     </section>
   );
 };
