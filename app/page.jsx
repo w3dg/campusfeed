de4fc73654f2acc1b9cccc4167331d2827584695
Sahil Choudhary <sahil844101@gmail.com>
2025-02-19 21:14:13 +0530

    Fixed some spacing

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
 import FooterSection from "@/components/sections/FooterSection";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutUsSection from "@/components/sections/AboutUsSection";
+import Features from "@/components/sections/Features";
 
 export default function Landing() {
   return (
     <div>
+      <HeroSection/>
+      <Features/>
+      <AboutUsSection/>
       <FooterSection/>
     </div>
   );
