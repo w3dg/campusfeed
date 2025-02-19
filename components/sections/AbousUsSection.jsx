fa2adc2a5762494493b85d764528811bb0bd0cc9
Supreet0 <supreetpatel2004@gmail.com>
2025-02-19 21:52:38 +0530

    About us Section Refactored

--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -1,26 +1,28 @@
 import teamData from "@/public/data/teamData.json";
-import ImageCard from '../fragments/Card';
+import ImageCard from "../fragments/Card";
 
 const AboutUsSection = () => {
   return (
-  <section className="py-10 bg-background px-10">
-      <div className="container mx-auto text-center w-full max-w-[800px] mt-8">
-        <h1 className="text-5xl font-bold text-gray-900 mb-6">About Us</h1>
-        <h1 className="text-xl text-gray-400 mb-20 text-center">Meet the developers and designers behind this awesome project</h1>
-        <div className="mt-6 flex flex-col items-center justify-center min-h-[50vh] py-2">
-                  <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:grid-cols-4">
-                  {teamData.map((member) => (
-            <ImageCard
-              key={member.id}
-              title={member.title}
-              subtitle={member.subtitle}
-              image={member.image}
-              github={member.github}
-            />
-          ))}
-                  </div>
-                </div>
+    <section className="bg-background px-10 py-10">
+      <div className="container mx-auto mt-4 w-full max-w-[800px] text-center">
+        <h1 className="mb-6 text-5xl font-bold text-gray-900">About Us</h1>
+        <h3 className="text-7 mt-6 font-sans text-slate-600">
+          Meet the developers and designers behind this awesome project.
+        </h3>
+        <div className="mt-6 flex min-h-[50vh] flex-col items-center justify-center py-2">
+          <div className="grid grid-cols-5 gap-10 md:grid-cols-3 lg:grid-cols-4">
+            {teamData.map((member) => (
+              <ImageCard
+                key={member.id}
+                title={member.title}
+                subtitle={member.subtitle}
+                image={member.image}
+                github={member.github}
+              />
+            ))}
           </div>
+        </div>
+      </div>
     </section>
   );
 };
