84cc2d1a6da3785e3e276e70b09de3b6a04280b0
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-19 20:19:11 +0530

    feat: Update About Us section background color for improved styling

--- components/sections/AbousUsSection.jsx
+++ components/sections/AbousUsSection.jsx
@@ -3,7 +3,7 @@ import ImageCard from '../fragments/Card';
 
 const AboutUsSection = () => {
   return (
-    <section className="py-10 bg-gray-100 px-10">
+    <section className="py-10 bg-background px-10">
       <div className="container mx-auto text-center w-full max-w-[800px] mt-8">
         <h1 className="text-5xl font-bold text-gray-900 mb-6">About Us</h1>
         <h1 className="text-xl text-gray-400 mb-20 text-center">Meet the developers and designers behind this awesome project</h1>
