46b6eafbaf816e41f4c654fa86c4bb6c93ca8c9a
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-19 20:30:54 +0530

    feat: Refactor ImageCard component for improved className handling and layout

--- components/fragments/Card.jsx
+++ components/fragments/Card.jsx
@@ -5,8 +5,8 @@ const ImageCard = ({ title, subtitle = "subtext", image, github }) => {
   return (
     <Card
       isPressable
-      className="bg-gradient-to-b from-slate-800 to-neutral-100 select-none group shrink-0 w-[160px] snap-center h-full rounded-xl hover:scale-105 transform-all duration-200 ease-in-out"
       shadow="lg"
+      classNames={{base:"bg-gradient-to-b from-slate-800 to-neutral-100 select-none group shrink-0 w-[160px] rounded-xl hover:scale-105 transform-all duration-200 ease-in-out h-full"}}
     >
       <CardBody className="overflow-visible p-2">
         <div className="flex aspect-square overflow-hidden rounded-xl items-center justify-center">
@@ -18,7 +18,7 @@ const ImageCard = ({ title, subtitle = "subtext", image, github }) => {
           />
         </div>
       </CardBody>
-      <CardFooter className="justify-between flex flex-row text-neutral-900 w-full">
+      <CardFooter className="justify-between flex flex-row text-neutral-900 w-full h-full items-end">
         <div className="flex flex-col w-[75%] gap-2 items-center justify-start">
           <h1 className="text-left font-bold text-md w-full">{title}</h1>
           <h1 className="text-left text-sm w-full">{subtitle}</h1>
