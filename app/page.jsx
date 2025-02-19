6e3757f93eb4ada5df8ccf0703a8579a9756fa6c
Sahil Choudhary <sahil844101@gmail.com>
2025-02-19 21:06:33 +0530

    Fixing again :)

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
-import HeroSection from "@/components/sections/HeroSection";
-import AboutUsSection from "@/components/sections/AbousUsSection";
-import Features from "@/components/sections/Features";
+import FooterSection from "@/components/sections/FooterSection";
 
 export default function Landing() {
   return (
     <div>
-      <HeroSection/>
-      <Features/>
-      <AboutUsSection/>
+      <FooterSection/>
     </div>
   );
 }
