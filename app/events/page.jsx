b3db1ead9245f6b3c1ee12c18c19c682af1f960c
Sarthak Sachdev <sarthak.tc737@gmail.com>
2025-02-20 20:04:27 +0530

    Event

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,12 +1,141 @@
-'use client'
-import EventCard from '@/components/fragments/EventCard'
-import NavBar from '@/components/fragments/NavBar'
-import Image from 'next/image'
-import { useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+"use client";
+import EventCard from "@/components/fragments/EventCard";
+import NavBar from "@/components/fragments/NavBar";
+import Image from "next/image";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const events = [
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+  {
+    eventPoster: "EventDemo1.svg",
+    eventName: "KODESPHERE - The Flagship Event",
+    eventLocation: "Campus-13",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 200.0,
+  },
+  {
+    eventPoster: "EventDemo2.svg",
+    eventName: "SPOTLIGHT - The Flagship Event",
+    eventLocation: "Campus-14",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 300.0,
+  },
+  {
+    eventPoster: "EventDemo3.svg",
+    eventName: "STEP IT UP - The Flagship Event",
+    eventLocation: "Campus-15",
+    eventDate: "Sat-25-Mar",
+    eventPrice: 400.0,
+  },
+];
 
 export default function Home() {
-  const [search, setSearch] = useState(false)
+  const [search, setSearch] = useState(false);
   return (
     <>
       <NavBar />
@@ -27,7 +156,7 @@ export default function Home() {
                 className="bg-transparent pr-3 rounded-md placeholder:text-black/45 placeholder:text-sm placeholder:font-light focus:outline-none w-[95%]"
               />
               <Image
-                src={'/SearchIcon.svg'}
+                src={"/SearchIcon.svg"}
                 height={20}
                 width={20}
                 alt="search"
@@ -43,7 +172,7 @@ export default function Home() {
                 className="mr-4 rounded-full bg-[#D3DEE3] p-2 md:hidden"
               >
                 <Image
-                  src={'/SearchIcon.svg'}
+                  src={"/SearchIcon.svg"}
                   height={20}
                   width={20}
                   alt="search"
@@ -54,12 +183,12 @@ export default function Home() {
               <motion.div className="relative md:hidden">
                 <motion.input
                   initial={{ width: 0, opacity: 0 }}
-                  animate={{ width: 'auto', opacity: 1 }}
+                  animate={{ width: "auto", opacity: 1 }}
                   exit={{ width: 0, opacity: 0 }}
                   transition={{ duration: 0.3 }}
                   placeholder="Search "
                   type="text"
-                  className="bg-[#D3DEE3] py-1 pl-3 pr-8 rounded-2xl placeholder:text-black/45 placeholder:text-sm placeholder:font-light focus:outline-none"
+                  className="bg-[#D3DEE3] py-1 pl-3 pr-8 border-2 rounded-2xl placeholder:text-black/45 placeholder:text-sm placeholder:font-light focus:outline-none"
                 />
                 <button
                   onClick={() => setSearch(false)}
@@ -85,20 +214,11 @@ export default function Home() {
           </AnimatePresence>
         </header>
         <div className="mt-3 px-0 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-2 md:gap-x-4 gap-y-10">
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
+          {events.map((event, index) => (
+            <EventCard event={event} key={index} />
+          ))}
         </div>
       </section>
     </>
-  )
+  );
 }
