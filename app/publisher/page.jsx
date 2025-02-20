d4e151725b88b206479c0d91a9a18291203b7482
Supreet0 <supreetpatel2004@gmail.com>
2025-02-20 15:12:45 +0530

    Minor changes in the publisher page

--- app/publisher/page.jsx
+++ app/publisher/page.jsx
@@ -50,10 +50,10 @@ const PublisherPage = () => {
   const { handleChange, handleSubmit } = detailsForm();
 
   return (
-    <div className="min-h-screen w-full bg-gradient-to-br from-[#e8f5e9] via-[#e3f2fd] to-[#f3e5f5] px-4 py-10">
-      <Card className="mx-auto w-full max-w-4xl items-center justify-center rounded-2xl bg-white/80 p-6 shadow-2xl backdrop-blur-sm">
+    <div className="min-h-screen w-full bg-gradient-to-br from-[#e8f5e9] via-[#e3f2fd] to-[#f3e5f5] px-2 py-10">
+      <Card className="mx-auto w-full max-w-4xl items-center justify-center rounded-2xl bg-[#fcfdfd] p-6 shadow-2xl backdrop-blur-sm">
         <Form onSubmit={handleSubmit}>
-          <CardHeader className="items-center space-x-2 divide-x-2 divide-gray-300">
+          <CardHeader className="items-center space-x-2 divide-x-2 divide-[#6DA27D]">
             <Image
               src="navlogo.svg"
               width={200}
@@ -63,7 +63,7 @@ const PublisherPage = () => {
             />
             <span className="pl-3 text-base font-normal">Details Form</span>
           </CardHeader>
-          <h3 className="mb-2 font-medium">Publisher Details</h3>
+          <h3 className="font-medium">Publisher Details</h3>
           <Divider className="mb-4 bg-[#6DA27D]" />
           <div className="mb-4 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             <Input
@@ -96,6 +96,7 @@ const PublisherPage = () => {
               label="Phone Number"
               variant="bordered"
               onChange={handleChange}
+              isRequired
             />
             <Input
               type="email"
@@ -137,7 +138,7 @@ const PublisherPage = () => {
             />
             <DateRangePicker
               name="duration"
-              label="Duration"
+              label="Start Date - End Date"
               variant="bordered"
               onChange={handleChange}
               isRequired
@@ -158,15 +159,7 @@ const PublisherPage = () => {
               isRequired
             />
           </div>
-          <div className="mb-4 grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
-            <Textarea
-              name="description"
-              label="Description"
-              variant="bordered"
-              isRequired
-              onChange={handleChange}
-              rows="4"
-            />
+          <div className="mb-4 grid w-full grid-cols-1 gap-8 lg:grid-cols-1">
             <Input
               type="url"
               name="image"
@@ -175,6 +168,14 @@ const PublisherPage = () => {
               icon={<FaUpload />}
               onChange={handleChange}
               isRequired
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              variant="bordered"
+              isRequired
+              onChange={handleChange}
+              rows="4"
             />
           </div>
           <Divider className="mb-4 bg-[#6DA27D]" />
