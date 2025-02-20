456085b4bebfdf307567d4f00a399697dba987ef
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-20 18:59:46 +0530

    Check

--- components/fragments/EventCard.jsx
+++ components/fragments/EventCard.jsx
@@ -2,30 +2,34 @@ import Image from 'next/image'
 
 const EventCard = () => {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col w-full max-w-[300px]">
       <Image
         src={'/EventDemo.svg'}
         alt="event"
-        width={150}
-        height={150}
-        className="h-[150px] w-[150px] md:w-[180px] md:h-[180px] lg:w-[250px] lg:h-[250px] object-contain border-[5px] border-[D3DEE3] rounded-2xl"
+        width={250}
+        height={250}
+        className="w-full aspect-square border-[3px] sm:border-[4px] md:border-[5px] border-[#D3DEE3] rounded-2xl object-contain"
       />
-      <div className="flex  mt-2 gap-1">
-        <div className="flex flex-col items-center text-center  rounded-md ">
-          <p className="text-[9px] bg-[#8690B1] px-3 py-1 rounded-t-md text-[#FCFDFD] min-w-full">
+      <div className="flex mt-3 gap-2 w-full">
+        <div className="flex flex-col items-center text-center rounded-md min-w-[50px]">
+          <p className="text-[10px] sm:text-xs bg-[#8690B1] px-3 py-1 rounded-t-md text-[#FCFDFD] w-full">
             Sat
           </p>
-          <p className="text-[14px] px-3 bg-[#E7EDF0] min-w-fit">27</p>
-          <p className="text-[10px] px-3 rounded-b-md bg-[#E7EDF0] min-w-full">
+          <p className="text-base sm:text-lg font-medium px-3 bg-[#E7EDF0] w-full">
+            27
+          </p>
+          <p className="text-[10px] sm:text-xs px-3 rounded-b-md bg-[#E7EDF0] w-full">
             Mar
           </p>
         </div>
-        <div className="flex flex-col text-[12px] ">
-          <p className="font-medium line-clamp-1 pr-3">
+        <div className="flex flex-col justify-center flex-1">
+          <p className="font-medium text-sm sm:text-base line-clamp-1">
             KODESPHERE - The Flagship Event
           </p>
-          <p className="font-medium">Campus 15</p>
-          <p className="text-black/50"> Rs.200</p>
+          <p className="font-medium text-xs sm:text-sm text-gray-700">
+            Campus 15
+          </p>
+          <p className="text-xs sm:text-sm text-black/60">Rs.200</p>
         </div>
       </div>
     </div>
