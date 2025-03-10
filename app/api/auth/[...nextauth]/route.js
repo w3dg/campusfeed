850fb1d0fb579b9da0e51a0114abed2f76d22df2
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-10 22:25:50 +0530

    fix(auth): correct typo in clientSecret property for Google provider
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { signIn } from "next-auth/react";
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clinetSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   session: {
