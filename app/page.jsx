bc4482f3bb6b488be522e480cd0d74541f75095d
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-11 09:34:40 +0530

    feat(Landing): implement session-based loading state for rendering sections
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,26 @@
-import HeroSection from "@/components/sections/HeroSection";
+"use client";
+
 import AboutUsSection from "@/components/sections/AbousUsSection";
 import Features from "@/components/sections/Features";
 import Footer from "@/components/sections/Footer";
+import HeroSection from "@/components/sections/HeroSection";
+import { useSession } from "next-auth/react";
 
 export default function Landing() {
+  const session = useSession();
+
   return (
     <div>
-      <HeroSection />
-      <Features />
-      <AboutUsSection />
-      <Footer />
+      {session.status === "loading" ? (
+        <></>
+      ) : (
+        <>
+          <HeroSection />
+          <Features />
+          <AboutUsSection />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
