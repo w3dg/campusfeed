d93dc43e88ee698271ee7f850140b84d2e5b0f5c
w3dg <51162936+w3dg@users.noreply.github.com>
2025-03-12 23:25:15 +0530

    feat(publisher-form): add sending event details to telegram chat and response to frontend

--- app/publisher/action.js
+++ app/publisher/action.js
@@ -2,16 +2,30 @@
 import { EventSchema } from "../../lib/schema";
 
 export async function addEvent(formData) {
-  const event = EventSchema.safeParse(formData);
+  const eventData = EventSchema.safeParse(formData);
 
-  if (!event.success) {
+  if (!eventData.success) {
+    console.log("error", eventData.error.errors);
     return {
-      status: 400,
-      body: {
-        error: event.error.errors,
-      },
+      success: false,
+      message: "Invalid data",
     };
   }
 
   // send data via graphql
+
+  try {
+    const text = JSON.stringify(eventData.data, null, 2);
+    const POST_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${text}`;
+    await fetch(POST_URL);
+    return {
+      success: true,
+      message: "Event submitted successfully for review",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Something went wrong",
+    };
+  }
 }
--- app/publisher/page.jsx
+++ app/publisher/page.jsx
@@ -18,12 +18,15 @@ import {
 import { formatISO } from "date-fns";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { EventSchema } from "../../lib/schema";
+import { addEvent } from "./action";
+import { toast, Toaster } from "sonner";
 
 const PublisherForm = () => {
   const formRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const {
     register,
     control,
@@ -33,8 +36,20 @@ const PublisherForm = () => {
     resolver: zodResolver(EventSchema),
   });
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log("submitted", data);
+    setIsSubmitting(true);
+    const result = await addEvent(data);
+    console.log(result);
+
+    if (result.success) {
+      formRef.current.reset();
+      setIsSubmitting(false);
+      toast.success(result.message);
+    } else {
+      setIsSubmitting(false);
+      toast.error(result.message);
+    }
   };
 
   const ControlledDatePicker = ({ name: formInputName, label }) => {
@@ -82,186 +97,193 @@ const PublisherForm = () => {
   };
 
   return (
-    <div
-      id="publisher-page"
-      className="min-h-screen w-full bg-gradient-to-br from-[#e8f5e9] via-[#e3f2fd] to-[#f3e5f5] px-2 py-10"
-    >
-      <Card className="mx-auto w-full max-w-4xl items-center justify-center rounded-2xl bg-[#fcfdfd] p-6 shadow-2xl backdrop-blur-sm">
-        <Form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
-          <CardHeader className="items-center space-x-2 divide-x-2 divide-[#6DA27D]">
-            <Image
-              src="navlogo.svg"
-              width={200}
-              height={200}
-              alt="logo"
-              className="-ml-4 pr-2"
-            />
-            <span className="pl-3 text-base font-normal">Details Form</span>
-          </CardHeader>
-          <h3 className="font-medium">Publisher Details</h3>
-          <Divider className="mb-4 bg-[#6DA27D]" />
-          <div className="mb-4 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <Input
-              {...register("firstName")}
-              type="text"
-              name="firstName"
-              label="First Name"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.firstName}
-              errorMessage="Enter a valid First Name"
-            />
-            <Input
-              {...register("lastName")}
-              type="text"
-              name="lastName"
-              label="Last Name"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.lastName}
-              errorMessage="Enter a valid Last Name"
-            />
-            <Input
-              {...register("position")}
-              type="text"
-              name="position"
-              label="Position"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.position}
-              errorMessage="Enter a valid Position"
-            />
-            <Input
-              {...register("phone")}
-              type="text"
-              name="phone"
-              label="Phone Number"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.phone}
-              errorMessage="Enter a valid Phone Number"
-            />
-            <Input
-              {...register("email")}
-              type="email"
-              name="email"
-              label="Email ID"
-              variant="bordered"
-              className="sm:col-span-2"
-              isRequired
-              isInvalid={!!errors.email}
-              errorMessage="Enter a valid Email"
-            />
-          </div>
+    <>
+      <Toaster richColors position="top-center" />
+      <div
+        id="publisher-page"
+        className="min-h-screen w-full bg-gradient-to-br from-[#e8f5e9] via-[#e3f2fd] to-[#f3e5f5] px-2 py-10"
+      >
+        <Card className="mx-auto w-full max-w-4xl items-center justify-center rounded-2xl bg-[#fcfdfd] p-6 shadow-2xl backdrop-blur-sm">
+          <Form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
+            <CardHeader className="items-center space-x-2 divide-x-2 divide-[#6DA27D]">
+              <Image
+                src="navlogo.svg"
+                width={200}
+                height={200}
+                alt="logo"
+                className="-ml-4 pr-2"
+              />
+              <span className="pl-3 text-base font-normal">Details Form</span>
+            </CardHeader>
+            <h3 className="font-medium">Publisher Details</h3>
+            <Divider className="mb-4 bg-[#6DA27D]" />
+            <div className="mb-4 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              <Input
+                {...register("firstName")}
+                type="text"
+                name="firstName"
+                label="First Name"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.firstName}
+                errorMessage="Enter a valid First Name"
+              />
+              <Input
+                {...register("lastName")}
+                type="text"
+                name="lastName"
+                label="Last Name"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.lastName}
+                errorMessage="Enter a valid Last Name"
+              />
+              <Input
+                {...register("position")}
+                type="text"
+                name="position"
+                label="Position"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.position}
+                errorMessage="Enter a valid Position"
+              />
+              <Input
+                {...register("phone")}
+                type="text"
+                name="phone"
+                label="Phone Number"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.phone}
+                errorMessage="Enter a valid Phone Number"
+              />
+              <Input
+                {...register("email")}
+                type="email"
+                name="email"
+                label="Email ID"
+                variant="bordered"
+                className="sm:col-span-2"
+                isRequired
+                isInvalid={!!errors.email}
+                errorMessage="Enter a valid Email"
+              />
+            </div>
 
-          <h3 className="font-medium">Event Details</h3>
-          <Divider className="mb-4 bg-[#6DA27D]" />
-          <div className="mb-4 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <Input
-              {...register("title")}
-              type="text"
-              name="title"
-              label="Event Title"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.title}
-              errorMessage="Enter a valid Title"
-            />
-            <Input
-              {...register("school")}
-              type="text"
-              name="school"
-              label="Organising School/Society"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.school}
-              errorMessage="Enter a valid School/Society"
-            />
-            <Input
-              {...register("venue")}
-              type="text"
-              name="venue"
-              label="Venue"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.venue}
-              errorMessage="Enter a valid Venue"
-            />
+            <h3 className="font-medium">Event Details</h3>
+            <Divider className="mb-4 bg-[#6DA27D]" />
+            <div className="mb-4 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              <Input
+                {...register("title")}
+                type="text"
+                name="title"
+                label="Event Title"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.title}
+                errorMessage="Enter a valid Title"
+              />
+              <Input
+                {...register("school")}
+                type="text"
+                name="school"
+                label="Organising School/Society"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.school}
+                errorMessage="Enter a valid School/Society"
+              />
+              <Input
+                {...register("venue")}
+                type="text"
+                name="venue"
+                label="Venue"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.venue}
+                errorMessage="Enter a valid Venue"
+              />
 
-            {/* DateInput isnt compatible with register, we wrap it in our own controlled input */}
+              {/* DateInput isnt compatible with register, we wrap it in our own controlled input */}
 
-            <ControlledDatePicker name={"startDate"} label={"Start Date"} />
-            <ControlledDatePicker name={"endDate"} label={"End Date"} />
+              <ControlledDatePicker name={"startDate"} label={"Start Date"} />
+              <ControlledDatePicker name={"endDate"} label={"End Date"} />
 
-            <Input
-              {...register("socialLinks", {
-                required: false,
-              })}
-              defaultValue=""
-              validate={(value) => {
-                if (!value) return null;
+              <Input
+                {...register("socialLinks", {
+                  required: false,
+                })}
+                defaultValue=""
+                validate={(value) => {
+                  if (!value) return null;
 
-                // check if socialLinks is a valid url
-                const urlRegex =
-                  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+                  // check if socialLinks is a valid url
+                  const urlRegex =
+                    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
 
-                if (!urlRegex.test(value)) {
-                  return "Enter a valid Social Media Link";
-                }
+                  if (!urlRegex.test(value)) {
+                    return "Enter a valid Social Media Link";
+                  }
 
-                return null;
-              }}
-              type="text"
-              name="socialLinks"
-              label="Social Media Link"
-              variant="bordered"
-              errorMessage="Enter a valid Social Media Link"
-            />
-          </div>
-          <div className="mb-4 grid w-full grid-cols-1 gap-8 lg:grid-cols-1">
-            <Input
-              {...register("registrationLinks")}
-              type="text"
-              name="registrationLinks"
-              label="Registration Link"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.registrationLinks}
-              errorMessage="Enter a valid Registration Link"
-            />
-            <Input
-              {...register("image")}
-              type="url"
-              name="image"
-              label="Poster Image URL"
-              variant="bordered"
-              isRequired
-              isInvalid={!!errors.image}
-              errorMessage="Enter a valid Image URL"
-            />
-            <Textarea
-              {...register("description")}
-              name="description"
-              label="Description"
-              variant="bordered"
-              isRequired
-              rows="4"
-              isInvalid={!!errors.description}
-              errorMessage="Enter a valid Description"
-            />
-          </div>
-          <Divider className="mb-4 bg-[#6DA27D]" />
-          <div className="jsutify-between flex w-full flex-col gap-4 sm:flex-row">
-            <Button type="button" variant="bordered" className="w-full">
-              Cancel
-            </Button>
-            <Button type="submit" className="w-full bg-[#6DA27D] text-white">
-              Confirm
-            </Button>
-          </div>
-        </Form>
-      </Card>
-    </div>
+                  return null;
+                }}
+                type="text"
+                name="socialLinks"
+                label="Social Media Link"
+                variant="bordered"
+                errorMessage="Enter a valid Social Media Link"
+              />
+            </div>
+            <div className="mb-4 grid w-full grid-cols-1 gap-8 lg:grid-cols-1">
+              <Input
+                {...register("registrationLinks")}
+                type="text"
+                name="registrationLinks"
+                label="Registration Link"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.registrationLinks}
+                errorMessage="Enter a valid Registration Link"
+              />
+              <Input
+                {...register("image")}
+                type="url"
+                name="image"
+                label="Poster Image URL"
+                variant="bordered"
+                isRequired
+                isInvalid={!!errors.image}
+                errorMessage="Enter a valid Image URL"
+              />
+              <Textarea
+                {...register("description")}
+                name="description"
+                label="Description"
+                variant="bordered"
+                isRequired
+                rows="4"
+                isInvalid={!!errors.description}
+                errorMessage="Enter a valid Description"
+              />
+            </div>
+            <Divider className="mb-4 bg-[#6DA27D]" />
+            <div className="jsutify-between flex w-full flex-col gap-4 sm:flex-row">
+              <Button type="button" variant="bordered" className="w-full">
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="w-full bg-[#6DA27D] text-white"
+                isDisabled={isSubmitting}
+              >
+                Confirm
+              </Button>
+            </div>
+          </Form>
+        </Card>
+      </div>
+    </>
   );
 };
 
