d133694e93d5ce1fea2caeddd3e588033db40ee9
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-13 21:24:25 +0530

    feat: Configure remote image patterns and add sonner dependency
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
